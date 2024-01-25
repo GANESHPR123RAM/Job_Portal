5c6d51798ec2890fb1b0916d38f5f3790c95819e
ganesh prajapati <ganeshprajapati1042001@gmail.com>
2024-01-25 16:32:28 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import HomePage from './Components/Homepage/HomePage';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <HomePage/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
